8deff65cdecafc5969b8c23d622458045f9e4321
mohammad najm <mohammadnajm75@gmail.com>
2022-08-12 20:32:00 +0430

    add increment and decrement

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ var io = require('socket.io-client');
 const socket = io.connect('http://localhost:3000', { reconnect: true });
 
 
-exports.initial = (ApiKey) => {
-    socket.emit("setApiKey", ApiKey)
-
+exports.initial = (ApiKey , callback) => {
+    if(typeof(ApiKey) == "string"){
+        socket.emit("setApiKey", ApiKey)
+        callback("Initialize is successful.")
+    }else{
+        callback(new Error("ApiKey must be a String!"))
+    }
 }
-exports.sendmetric = (names , count=1)=> {
-    console.log(names)
-    socket.emit('save_metrics', { name: names ,count :count})
+
+exports.increment = (metric = null , count = 1) => {
+    if(typeof(metric) == 'string' && typeof(count) == 'number' && count > 0){
+        socket.emit('increment', { metric,count})
+    }
+}
+
+exports.decrement =  (metric = null , count = 1) => {
+    if(typeof(metric) == 'string' && typeof(count) == 'number' && count > 0){
+        socket.emit('decrement', { metric,count})
+    }
 }
 
 
