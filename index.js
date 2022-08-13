fb96b37e2f477cf7df90c0cccb887c1a59960236
mohammad najm <mohammadnajm75@gmail.com>
2022-08-13 12:06:12 +0430

    add class for SocketCli and fix ApiKey bug after disconnect

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 var io = require('socket.io-client');
 const socket = io.connect('http://localhost:3000', { reconnect: true });
 
-
-exports.initial = (ApiKey) => {
-    if(typeof(ApiKey) == "string"){
-        socket.emit("setApiKey", ApiKey)
+class SocketCli {
+    constructor() {
+        this.ApiKey = null
     }
+
+    initial(ApiKey) {
+        if (typeof (ApiKey) == "string") {
+            this.ApiKey = ApiKey
+            socket.emit("setApiKey", ApiKey)
+        }
+    }
+
+    increment(metric = null, count = 1) {
+        if (typeof (metric) == 'string' && typeof (count) == 'number' && count > 0) {
+            socket.emit('increment', { metric, count, ApiKey: this.ApiKey })
+        }
+    }
+    decrement(metric = null, count = 1) {
+        if (typeof (metric) == 'string' && typeof (count) == 'number' && count > 0) {
+            socket.emit('decrement', { metric, count, ApiKey: this.ApiKey })
+        }
+    }
+
 }
 
-exports.increment = (metric = null , count = 1) => {
-    if(typeof(metric) == 'string' && typeof(count) == 'number' && count > 0){
-        socket.emit('increment', { metric,count})
-    }
-}
-
-exports.decrement =  (metric = null , count = 1) => {
-    if(typeof(metric) == 'string' && typeof(count) == 'number' && count > 0){
-        socket.emit('decrement', { metric,count})
-    }
-}
+module.exports = new SocketCli;
