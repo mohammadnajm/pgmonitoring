eb3f6fab2b6a6df2f80a4996610652fac2f6305e
mohammad <mohammadnajm75@gmail.com>
2022-12-19 12:09:03 +0330

    change localhost to 0.0.0.0 and ApiKey to apiKey

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var io = require('socket.io-client');
-const socket = io.connect('http://localhost:3000', { reconnect: true });
+const socket = io.connect('http://0.0.0.0:3000', { reconnect: true });
 
 class SocketCli {
     constructor() {
-        this.ApiKey = null
+        this.apiKey = null
     }
 
-    initial(ApiKey) {
-        if (typeof (ApiKey) == "string") {
-            this.ApiKey = ApiKey
-            socket.emit("setApiKey", ApiKey)
+    initial(apiKey) {
+        if (typeof (apiKey) == "string") {
+            this.apiKey = apiKey
+            socket.emit("setApiKey", apiKey)
         }
     }
 
     increment(metric = null, count = 1) {
         if (typeof (metric) == 'string' && typeof (count) == 'number' && count > 0) {
-            socket.emit('increment', { metric, count, ApiKey: this.ApiKey })
+            socket.emit('increment', { metric, count, apiKey: this.apiKey })
         }
     }
     decrement(metric = null, count = 1) {
         if (typeof (metric) == 'string' && typeof (count) == 'number' && count > 0) {
-            socket.emit('decrement', { metric, count, ApiKey: this.ApiKey })
+            socket.emit('decrement', { metric, count, apiKey: this.apiKey })
         }
     }
 
